8b3faae0c9f4212e4653a9b01b55574557abda7b
Hugo Flores Garcia <hf01049@georgisouthern.edu>
2021-01-06 11:14:32 -0600

    add preprocess_dataset fn

--- instrument_recognition/preprocess.py
+++ instrument_recognition/preprocess.py
@@ -1,10 +1,16 @@
+from pathlib import Path
+import os
+
 import numpy as np
+import tqdm
 import pytorch_lightning as pl
+import torch
 
 import torchopenl3
 import audio_utils as au
 
 import instrument_recognition as ir
+import instrument_recognition.datasets 
 from instrument_recognition import utils
 
 class OpenL3Preprocessor(pl.LightningModule):
@@ -12,6 +18,7 @@ class OpenL3Preprocessor(pl.LightningModule):
     def __init__(self, model_name: str = 'openl3-mel256-6144-music', cuda_device=None):
         super().__init__()
         self.save_hyperparameters()
+        self.name = model_name
         _, input_repr, embedding_size, content_type = model_name.split('-')
         self.embedding_model = torchopenl3.OpenL3Embedding(input_repr=input_repr, 
                                                     embedding_size=int(embedding_size), 
@@ -42,3 +49,65 @@ class OpenL3Preprocessor(pl.LightningModule):
                                     sample_rate=sr, device=self.cuda_device)
         
         return embeddings
+
+def preprocess_dataset(name: str, model_name: str, batch_size: int, num_workers: int, device: int = None):
+    _, input_repr, embedding_size, content_type = model_name.split('-')
+    model = torchopenl3.OpenL3Embedding(input_repr=input_repr, embedding_size=int(embedding_size), 
+                                        content_type=content_type, pretrained=True)
+    if device is not None:
+        model = model.to(device)
+
+    dm = ir.datasets.DataModule(name, batch_size, num_workers)
+    dm.setup()
+
+    loaders = ((dm.train_dataloader(), True, dm.train_data.root_dir), 
+                (dm.val_dataloader(), True,  dm.val_data.root_dir))
+
+    for loader, augment,  root_dir in loaders:
+        pbar = tqdm.tqdm(loader)
+        for batch in pbar:
+            # get all inputs
+            X = batch['X']
+
+            # get embedding
+            X = X.view(-1, 10, 1, 48000)
+            X = X.permute(1, 0, 2, 3)
+            X_embedded = []
+            for X_step in X:
+                if device is not None:
+                    X_step = X_step.to(device)
+                X_step = model(X_step).detach().cpu().numpy()
+                X_embedded.append(X_step)
+            X = np.stack(X_embedded)
+            X = X.transpose(1, 0, 2)
+
+            # now, iterate through paths
+            for x, path in zip(X, batch['path_to_audio']):
+                pbar.set_description(str(Path(path).stem))
+                cache_dir = ir.CACHE_DIR / model_name
+                path_to_cached_file = cache_dir / Path(path).relative_to(ir.DATA_DIR).with_suffix('.npy')
+
+                # save embeddings to cache
+                os.makedirs(path_to_cached_file.parent, exist_ok=True)
+                np.save(str(path_to_cached_file), x)
+
+if __name__ == "__main__":
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--num_workers', type=int, default=18)
+    parser.add_argument('--device', type=int, default=0)
+
+    args = parser.parse_args()
+    preprocess_dataset(args.name, args.model, args.batch_size, args.num_workers, args.device)
+
+    # for model in ('openl3-mel128-6144-music', 'openl3-mel256-6144-music',
+    #               'openl3-mel128-512-music', 'openl3-mel256-512-music',
+    #               'openl3-mel128-6144-env', 'openl3-mel256-6144-env',
+    #               'openl3-mel128-512-env', 'openl3-mel256-512-env'):
+    #     preprocess_dataset(args.name, model, args.batch_size, args.num_workers, args.device)
+    
